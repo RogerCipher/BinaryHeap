24f29024efc3d1d81ec18922bb185ca267273774
Rogério O. R. Chaves <61879622+RogerCipher@users.noreply.github.com>
2021-12-09 15:50:02 +0000

    Cmake stuff added from Clion

---
+++ cmake-build-debug/CMakeFiles/BinaryHeap.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinaryHeap.exe"
+  "BinaryHeap.exe.manifest"
+  "BinaryHeap.lib"
+  "BinaryHeap.pdb"
+  "CMakeFiles/BinaryHeap.dir/priorityQueues.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BinaryHeap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
